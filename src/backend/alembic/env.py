341ed220936b4925a8f71d8e75fb90659f50187d
Brian-Wahome <76201235+Brian-Wahome@users.noreply.github.com>
2025-02-05 18:40:40 +0000

    chore(alembic): update import

--- src/backend/alembic/env.py
+++ src/backend/alembic/env.py
@@ -19,7 +19,7 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 # Import your models here
-from src.backend.database import Base
+from database import Base
 
 target_metadata = Base.metadata
 
