9b9342d9c33a21328ba8b3ee7d72312100f6d78c
Brian-Wahome <76201235+Brian-Wahome@users.noreply.github.com>
2025-02-05 18:02:09 +0000

    feat(backend): add user table to orm

--- src/backend/database.py
+++ src/backend/database.py
@@ -2,6 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
+from enum import Enum
+from sqlalchemy import Column, String, UUID, DateTime, ForeignKey, Boolean, Enum, Text, JSON, TIMESTAMP
+from sqlalchemy.sql import func
+import uuid
+import datetime
+
 
 load_dotenv()
 
@@ -22,3 +28,21 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+class UserRole(Enum):
+    PATIENT = 'patient'
+    THERAPIST = 'Therapist'
+    ADMIN = 'admin'
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(UUID, primary_key=True, default=uuid.uuid4)
+    email = Column(String(255), unique=True, nullable=False)
+    role = Column(Enum(UserRole), nullable=False)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    last_updated_at = Column(TIMESTAMP(timezone=True), onupdate=func.now())
