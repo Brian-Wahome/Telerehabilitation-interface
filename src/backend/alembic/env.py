b2944fc87494cd6e7724e4c5ada2c1b426a93ad4
Brian-Wahome <76201235+Brian-Wahome@users.noreply.github.com>
2025-02-05 20:08:07 +0000

    chore(alembic): update import

--- src/backend/alembic/env.py
+++ src/backend/alembic/env.py
@@ -18,8 +18,8 @@ config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# Import your models here
-from database import Base
+# Import models here
+from orm import Base
 
 target_metadata = Base.metadata
 
