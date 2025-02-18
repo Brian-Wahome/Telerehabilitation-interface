bd6eafde07d7555149a684e789ea668f825bf55c
Brian-Wahome <76201235+Brian-Wahome@users.noreply.github.com>
2025-02-18 16:37:24 +0000

    feat(abstraction): add logging to abstraction

--- src/backend/abstraction.py
+++ src/backend/abstraction.py
@@ -1,19 +1,27 @@
 from sqlalchemy.orm import Session
 from contextlib import contextmanager
 from .orm import User
+from uuid import uuid4
+import structlog
 
 
 class BaseAbstraction:
     def __init__(self, db: Session):
         self.db = db
-
+        self.session_id = str(uuid4())
+        self.logger = structlog.getLogger(__name__).bind(
+            session_id=self.session_id,
+            layer="repository",
+        )
     @contextmanager
     def transaction(self):
         try:
             yield
             self.db.commit()
-        except Exception:
+            self.logger.debug("Database commit is successful")
+        except Exception as e:
             self.db.rollback()
+            self.logger.error(f"Error {e} raised when committing")
             raise
 
 
