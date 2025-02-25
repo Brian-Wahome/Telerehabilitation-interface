101f450866d5a6fc160cc3bbaa8688b9a8241018
Brian-Wahome <76201235+Brian-Wahome@users.noreply.github.com>
2025-02-25 19:37:20 +0000

    feat(abstraction): add session abstraction

--- src/backend/abstraction.py
+++ src/backend/abstraction.py
@@ -1,12 +1,12 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session as db_session
 from contextlib import contextmanager
-from .orm import User
+from .orm import User, Session
 from uuid import uuid4
 import structlog
 
 
 class BaseAbstraction:
-    def __init__(self, db: Session):
+    def __init__(self, db: db_session):
         self.db = db
         self.session_id = str(uuid4())
         self.logger = structlog.stdlib.get_logger(__name__).bind(
@@ -31,3 +31,10 @@ class UserAbstraction(BaseAbstraction):
         with self.transaction():
             user = User(**user)
             self.db.add(user)
+
+
+class SessionAbstraction(BaseAbstraction):
+    def create_session(self, session: dict) -> Session:
+        with self.transaction():
+            session = Session(**session)
+            self.db.add(session)
