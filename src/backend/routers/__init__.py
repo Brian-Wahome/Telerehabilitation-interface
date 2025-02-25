eb6616832f72f9063b7317bd7a25893d9d4dfecb
Brian-Wahome <76201235+Brian-Wahome@users.noreply.github.com>
2025-02-25 21:36:37 +0000

    refactor(router): add all routers in package

---
+++ src/backend/routers/__init__.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+from .users import router as users_router
+from .sessions import router as sessions_router
+
+router = APIRouter()
+router.include_router(users_router)
+router.include_router(sessions_router)
