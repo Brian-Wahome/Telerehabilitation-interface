2b17b390a439e5b78e4203bc066c2b2763083aff
Brian-Wahome <76201235+Brian-Wahome@users.noreply.github.com>
2025-02-25 20:30:31 +0000

    feat(dependency): add session dependency

--- src/backend/dependencies/services.py
+++ src/backend/dependencies/services.py
@@ -1,10 +1,16 @@
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from ..database import get_db
-from ..abstraction import UserAbstraction
+from ..abstraction import UserAbstraction, SessionAbstraction
 from ..service.users_service import UserService
+from ..service.sessions_service import SessionService
 
 
 def get_user_service(db: Session = Depends(get_db)) -> UserService:
     user_abstraction = UserAbstraction(db)
     return UserService(user_abstraction)
+
+
+def get_session_service(db: Session = Depends(get_db)) -> SessionService:
+    session_abstraction = SessionAbstraction(db)
+    return SessionService(session_abstraction)
