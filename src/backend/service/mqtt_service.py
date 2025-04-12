b55da8d4fca70e64307e1f0d969e5785158d3b6e
Brian-Wahome <76201235+Brian-Wahome@users.noreply.github.com>
2025-04-12 15:39:35 +0100

    feat(mqtt_service): add disconnect method

--- src/backend/service/mqtt_service.py
+++ src/backend/service/mqtt_service.py
@@ -1,18 +1,18 @@
-from typing import Dict
 import structlog
-from src.backend.abstraction import MQTTAbstraction
 from src.backend.mqtt import EMGMQTTClient
 
 
 class MQTTService:
     def __init__(self):
+        self.mqtt_client = None
         self.logger = structlog.get_logger(__name__)
+        self.mqtt_client
 
     def setup_mqtt_client(self, broker_host="localhost", broker_port=8083, username=None, password=None,
                           topic_pattern="emg/+/data", transport="websockets", client_id="default",
                           message_handler=None):
         try:
-            mqtt_client = EMGMQTTClient(
+            self.mqtt_client = EMGMQTTClient(
                 broker_host,
                 broker_port,
                 message_handler,
@@ -22,11 +22,18 @@ class MQTTService:
                 transport,
                 client_id
             )
-            mqtt_client.connect()
+            self.mqtt_client.connect()
             self.logger.info(
                 "MQTT client successfully connected to broker"
             )
         except Exception:
             raise
 
-
+    def disconnect_client(self, client_id="default"):
+        topic = "control/disconnect"
+        payload = {"client_id": client_id}
+        self.mqtt_client.publish(topic, payload)
+        self.logger.info(
+            "MQTT client successfully disconnected",
+            mqtt_client_id=client_id
+        )
