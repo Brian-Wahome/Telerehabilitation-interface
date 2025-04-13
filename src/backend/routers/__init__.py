4c827878c57626be5b4dfbcc0e8694addfceb51b
Brian-Wahome <76201235+Brian-Wahome@users.noreply.github.com>
2025-04-13 12:27:18 +0100

    feat(routers): add router

--- src/backend/routers/__init__.py
+++ src/backend/routers/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from .users import router as users_router
 from .sessions import router as sessions_router
+from .mqtt import router as mqtt_router
 
 router = APIRouter()
 router.include_router(users_router)
 router.include_router(sessions_router)
+router.include_router(mqtt_router)
