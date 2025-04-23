b76a76e8654c03a9e8623b9b82187cff2b4a1e6d
Brian-Wahome <76201235+Brian-Wahome@users.noreply.github.com>
2025-04-23 19:30:12 +0100

    feat(main): setup mqtt_client on app startup

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,14 +1,33 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 from src.config.logging_config import configure_logging
 from src.backend.routers import router
+from dependencies.services import mqtt_service, get_mqtt_abstraction
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Tele-rehabilitation Interface", on_startup=[configure_logging])
+message_handler = get_mqtt_abstraction()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    configure_logging()
+    mqtt_service.setup_mqtt_client(message_handler=message_handler)
+
+    yield  # The app runs during this time
+
+    # Shutdown
+    mqtt_service.disconnect_client(client_id="default")
+    mqtt_service.remove_client(client_id="default")
+
+
+app = FastAPI(title="Tele-rehabilitation Interface")
 
 # Configure CORS
 app.add_middleware(
@@ -21,6 +40,7 @@ app.add_middleware(
 
 # add routers to app
 app.include_router(router)
+
 
 @app.get("/")
 async def root():
